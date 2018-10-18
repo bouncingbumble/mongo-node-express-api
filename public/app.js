a1c99c0d007d022efb9bb9786cb5de41ef1d0e21
bouncingbumble <jordboudreau@gmail.com>
2018-10-18 10:38:12 -0700

    add get and post

--- public/app.js
+++ public/app.js
@@ -1,14 +1,36 @@
 $(document).ready(() => {
-    $.getJSON("/api/todos", data => {
-        addTodos(data);
+    $.getJSON("/api/todos")
+        .then(addTodos);
+
+    $('#todoInput').keypress(event => {
+        if(event.which == 13) {
+            createTodo();
+        }
     })
 });
 
 function addTodos(todos){
-    console.log(todos);
-    for (let todo of todos) {
-        listValue = document.createElement("li");
-        listValue.textContent = todo.name;
-        document.querySelector(".list").appendChild(listValue);
+    todos.forEach(todo => {
+        addTodo(todo);
+    })
+}
+
+function createTodo(){
+    var userInput = $('#todoInput').val();
+    $.post('/api/todos', { name: userInput })
+        .then(newTodo => {
+            $('#todoInput').val('');
+            addTodo(newTodo);
+        })
+        .catch(err => {
+            console.log(err);
+        })
+}
+
+function addTodo(todo){
+    var newTodo = $('<li class="task">' + todo.name + ' <span>x</span></li>');
+    if(todo.completed){
+        newTodo.addClass("done");
     }
+    $('.list').append(newTodo);
 }
