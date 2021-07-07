69b7873672c61b68aa1baa3168f7a38fcf5c247b
bouncingbumble <jordboudreau@gmail.com>
2021-07-07 11:43:50 -0700

    add all files

--- index.js
+++ index.js
@@ -1,18 +1,54 @@
-var express = require('express'),
-    app = express(),
-    port = 3000,
-    bodyParser = require('body-parser'),
-    todoRoutes = require('./routes');
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const server = require('http').createServer(app)
+const { v4: uuid } = require('uuid')
+const cors = require('cors')
+const port = process.env.PORT || 9000
+const bodyParser = require('body-parser')
+const schedule = require('node-schedule')
+const axios = require('axios')
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static(__dirname + '/views'));
-app.use(express.static(__dirname + '/public'));
+app.use(cors())
+app.use(bodyParser.json({ limit: '50mb' }))
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }))
 
-app.get('/', (req, res) => { res.sendFile('index.html') })
-app.use('/api/todos', todoRoutes);
+app.post('/', (req, res, next) => {
+    const { timeToSend, url, data } = req.body
+    data.CRON_SECRET_KEY = process.env.SECRET_KEY
+    console.log('starting timer')
+    console.log(timeToSend)
 
-//server
-app.listen(port, () => {
-    console.log('App is running on port ' + port);
+    const jobName = uuid()
+
+    const job = schedule.scheduleJob(
+        jobName,
+        new Date(timeToSend),
+        async function () {
+            try {
+                console.log('timer done')
+                console.log('hitting endpoint')
+                console.log(url)
+                console.log('sending back data')
+                console.log(data)
+                try {
+                    await axios.post(url, data)
+                } catch (error) {
+                    next(error)
+                }
+            } catch (err) {
+                return next(err)
+            }
+        }
+    )
+})
+
+app.use((req, res, next) => {
+    let err = new Error('Not Found')
+    err.status = 404
+    console.log(err)
+})
+
+server.listen(port, () => {
+    console.log('App is running on port ' + port)
 })
--- models/index.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-
-mongoose.set('debug', true);
-mongoose.connect('mongodb://admin:@ds113402.mlab.com:13402/todo-api');
-mongoose.Promise = Promise;
-
-module.exports.Todo = require('./todo');
-
